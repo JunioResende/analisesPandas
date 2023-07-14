40737cfff6c87d0c0744b346e884f444e4fcb955
JunioResende <junioresende23@gmail.com>
2023-07-14 18:29:42 -0300

    series

--- examples/series.py
+++ examples/series.py
@@ -11,16 +11,24 @@ arr = np.array([10, 20, 30])
 
 d = {'a': 10, 'b': 20, 'c': 30}
 
-# indices automaticos
+print('# indices automaticos')
 indice = pd.Series(labels)
-# imprime o conjunto de dados com indices iniciando de 0... de forma crescente
+print('# imprime o conjunto de dados com indices iniciando de 0... de forma crescente')
 print(indice)
 
-# indices atribuidos
+print('# indices atribuidos')
 indiceA = pd.Series(data=labels, index=minha_lista)
-# imprime o conjunto de dados com indices atribuidos manualmente
+print('# imprime o conjunto de dados com indices atribuidos manualmente')
 print(indiceA)
 
 dictionaryIndice = pd.Series(d)
-# dicionarios sao compostos por chave:valor nesse caso a chave se tornam indices automaticos
+print('# dicionarios sao compostos por chave:valor nesse caso a chave se tornam indices automaticos')
 print(dictionaryIndice)
+
+paises = ['Brasil', 'Argentina', 'Uruguai', 'Paraguai']
+indexPais = [1, 2, 3, 4]
+populacao = [564654881.3, 1688465.5, 1685321.16, 4549884.12]
+
+ser1 = pd.Series(index=indexPais, data=paises)
+
+print(ser1)
